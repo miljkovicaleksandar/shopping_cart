7e6cc4e20134c25501200e1ae40138bcb2f3d7ac
miljkovicaleksandar <fazzon96@gmail.com>
2019-10-02 19:49:52 +0200

    clear cart functionality added

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ class UI {
             <img src=${product.image} alt="product" class="product-img" >
             <button class="bag-btn" data-id=${product.id}>
                 <i class="fas fa-shopping-cart"></i>
-                add to bag
+                add to cart
             </button>
         </div>
         <h3>${product.title}queen bed</h3>
@@ -137,6 +137,33 @@ class UI {
         cartOverlay.classList.remove('transparentBcg');
         cartDom.classList.remove('showCart');
     }
+    cartLogic(){
+        // clear cart button
+        clearCartBtn.addEventListener('click', () =>{
+            this.clearCart();
+        });
+        // cart functionality
+    }
+    clearCart(){
+        let cartItems = cart.map(item => item.id);
+        cartItems.forEach(id => this.removeItem(id));
+        while(cartContent.children.length>0){
+            cartContent.removeChild(cartContent.children[0]);
+        }
+        this.hideCart();
+    }
+    removeItem(id){
+        cart = cart.filter(item => item.id !==id);
+        this.setCartValues(cart);
+        Storage.saveCart(cart);
+        let button = this.getSingleButton(id);
+        button.disabled = false;
+        button.innerHTML = '<i class="fas fa-shopping-cart"></i>add to cart';
+    }
+    getSingleButton(id){
+        return buttonsDom.find(button => button.dataset.id === id);
+        
+    }
 }
 
 
@@ -168,5 +195,6 @@ document.addEventListener("DOMContentLoaded", () => {
         Storage.saveProducts(products);
     }).then(() => {
         ui.getBagButtons();
+        ui.cartLogic()
     })
 })
